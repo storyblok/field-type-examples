e49fe4493c8fe74e6eb581d0c5e2761afce49146
Eunjae Lee <hey@eunjae.dev>
2022-11-24 16:03:40 +0100

    fix: update vue.config.js and eslint

--- tags/.eslintrc.js
+++ tags/.eslintrc.js
@@ -1,19 +1,57 @@
 module.exports = {
-    "root": true,
-
-    "env": {
-        "node": true
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  // 'parser': '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: '*.ts',
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {}, // Override rules as well for TS files. Basically any config option can be modified here
     },
+    {
+      files: '*.vue',
+      parser: 'vue-eslint-parser',
+      extends: [],
+      rules: {}, // Override rules as well for TS files. Basically any config option can be modified here
+    },
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier-vue/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue', 'prettier'],
 
-    "extends": [
-        "plugin:vue/essential",
-        "eslint:recommended",
-        '@vue/typescript'
+  rules: {
+    'prettier-vue/prettier': [
+      'warn',
+      {
+        bracketSpacing: true,
+        printWidth: 80,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        singleAttributePerLine: true,
+      },
     ],
-
-    "rules": {},
-
-    "parserOptions": {
-        "parser": '@typescript-eslint/parser'
-    },
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 }
