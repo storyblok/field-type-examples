e21ebc63f64097194bbd45e785ab4fb165f9cddd
johannes-lindgren <johannes-lindgren@hotmail.com>
2022-12-12 15:43:29 +0100

    EXT-827: simplified HMR without HTTPS

--- slider/vue.config.cjs
+++ slider/vue.config.cjs
@@ -19,23 +19,10 @@ const pages = process.env.NODE_ENV === 'production' ? (
 
 const publicUrl = 'http://localhost:8080'
 
-if (typeof publicUrl === 'undefined') {
-    console.warn('WARNING',
-      `To enable Hot Module Replacement (HMR), please specify the environmental variable PUBLIC_URL in .env.local.
-     You can use a local HTTPS proxy server or a secure tunnel, for example ngrok.`
-    )
-}
-const useLocalSslProxy = publicUrl?.startsWith('https://localhost')
-
 module.exports = {
     pages,
     configureWebpack: {
         entry: entry,
-        // output: {
-        //     path: __dirname + './features/communityDirectory',
-        //     publicPath: '/',
-        //     filename: 'bundle.[name].js'
-        // },
         output: {
             filename: '[name].js'
         },
@@ -57,12 +44,6 @@ module.exports = {
         extract: false
     },
     devServer: {
-        // Using a local https server
-        https: useLocalSslProxy ? {
-            key: fs.readFileSync('./certificates/localhost-key.pem'),
-            cert: fs.readFileSync('./certificates/localhost.pem'),
-        } : undefined,
-
         // Using secure tunnel
         public: publicUrl,
         disableHostCheck: true,
