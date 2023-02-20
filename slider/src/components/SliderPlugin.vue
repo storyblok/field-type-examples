436580e136308f4347ebca4a9480ca4e2145c87d
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-02-20 17:02:06 +0100

    EXT-1110: fix outbound marks

--- slider/src/components/SliderPlugin.vue
+++ slider/src/components/SliderPlugin.vue
@@ -1,7 +1,20 @@
+<template>
+  <HorizontalSlider
+    :value="selectedValue"
+    :set-value="(val) => setValue({ value: val })"
+    :min-value="minValue"
+    :max-value="maxValue"
+    :marks="filteredMarks"
+  />
+</template>
+
 <script>
 import { HorizontalSlider } from '@/components/HorizontalSlider'
 
 export default {
+  components: {
+    HorizontalSlider,
+  },
   props: {
     options: {
       type: Object,
@@ -16,44 +29,57 @@ export default {
       default: undefined,
     },
   },
+
   computed: {
+    minValue() {
+      return this.getMinValue()
+    },
+    maxValue() {
+      return this.getMaxValue()
+    },
+    filteredMarks() {
+      const marks = this.getMarks()
+      return this.filterInvalidMarks(marks)
+    },
+    selectedValue() {
+      return this.value?.value ?? this.defaultValue()
+    },
     defaultValue() {
       return this.options.defaultValue
         ? Number.parseFloat(this.options.defaultValue)
         : this.minValue
     },
-    minValue() {
+  },
+
+  created() {
+    if (!this.value?.value) {
+      this.setValue({ value: this.defaultValue })
+    }
+  },
+  methods: {
+    getMinValue() {
       return this.options.minValue
         ? Number.parseFloat(this.options.minValue)
         : 0
     },
-  },
-  created() {
-    const defaultValue = parseFloat(this.defaultValue)
-    if (!this.value?.value) {
-      this.setValue({ value: defaultValue })
-    }
-  },
-  render() {
-    const minValue = this.minValue
-    const maxValue = this.options.maxValue
-      ? Number.parseFloat(this.options.maxValue)
-      : 100
-    const value = this.value?.value || this.defaultValue
-    const marks =
-      typeof this.options.marks === 'undefined'
+    getMaxValue() {
+      return this.options.maxValue
+        ? Number.parseFloat(this.options.maxValue)
+        : 100
+    },
+    getMarks() {
+      return typeof this.options.marks === 'undefined'
         ? undefined
         : JSON.parse(this.options.marks)
-    return (
-      <HorizontalSlider
-        // archAngle={archAngle}
-        value={value}
-        setValue={(value) => this.setValue({ value })}
-        minValue={minValue}
-        maxValue={maxValue}
-        marks={marks}
-      />
-    )
+    },
+    filterInvalidMarks(marks) {
+      if (!Array.isArray(marks)) {
+        return marks
+      }
+      return marks.filter(
+        (mark) => mark > this.minValue && mark < this.maxValue,
+      )
+    },
   },
 }
 </script>
