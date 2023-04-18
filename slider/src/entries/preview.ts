615905b065a91cc445d51500496c0eac5909a7d0
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-18 21:11:14 +0200

    Renaming preview plugin

--- slider/src/entries/preview.ts
+++ slider/src/entries/preview.ts
@@ -2,7 +2,7 @@ import { loadPlugin } from '@/lib/loadPlugin'
 import SliderPlugin from '@/components/SliderPlugin.vue'
 
 loadPlugin(
-  'storyblok-slider',
+  'storyblok-slider-preview',
   {
     value: undefined,
   },
