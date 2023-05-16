d311211ce92e3215594f45ad685e02212385e2f7
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-16 16:32:48 +0200

    feat: title input field

--- code-block/src/components/CodeEditor/CodeEditor.tsx
+++ code-block/src/components/CodeEditor/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react'
+import { ChangeEventHandler, FunctionComponent } from 'react'
 import { CodeEditorContent } from './CodeEditorContent'
 import { withLength } from './withLength'
 import { toggleLine } from './toggleLine'
@@ -6,7 +6,7 @@ import { CodeMirror } from '../CodeMirror'
 import { gutterColorFromState } from './gutterColorFromState'
 import { mix } from './mix'
 import { backgroundColorFromState } from './backgroundColorFromState'
-import { sb_dark_blue } from '../../design-tokens'
+import { sb_dark_blue, sb_dark_blue_50, white } from '../../design-tokens'
 import { css } from '@emotion/react'
 
 /**
@@ -34,30 +34,84 @@ export const CodeEditor: FunctionComponent<{
       lineStates: toggleLine(lineStates, line),
     })
 
+  const setTitle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.currentTarget
+    setContent({
+      ...content,
+      title: value === '' ? undefined : value,
+    })
+  }
+
   return (
-    <CodeMirror
-      css={css(
-        content.lineStates.map((state, index) => ({
-          [`.cm-gutterElement:nth-child(${index + 2})`]: state !== '' && {
-            backgroundColor: mix(
-              backgroundColorFromState(state),
-              sb_dark_blue,
-              0.5,
-            ),
-            color: gutterColorFromState(state),
-          },
-          [`.cm-line:nth-child(${index + 1})`]: state !== '' && {
-            backgroundColor: mix(
-              backgroundColorFromState(state),
-              sb_dark_blue,
-              0.25,
-            ),
-          },
-        })),
-      )}
-      initialValue={code}
-      onChange={onChange}
-      onLineNumberClick={handleLineNumberClick}
-    />
+    <div
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'stretch',
+        borderRadius: '5px',
+        overflow: 'hidden',
+      })}
+    >
+      <div
+        css={css({
+          display: 'flex',
+          borderBottom: `1px solid rgb(141, 145, 159)`,
+          padding: '5px 15px',
+          backgroundColor: sb_dark_blue,
+          color: white,
+        })}
+      >
+        <input
+          css={css({
+            flex: 1,
+
+            color: 'inherit',
+            backgroundColor: 'inherit',
+            border: 'none',
+
+            margin: '0px',
+            fontSize: '12px',
+            fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+            lineHeight: '1.66',
+            letterSpacing: '0.03333em',
+
+            '&:focus': {
+              outline: 'none',
+            },
+            '&::placeholder': {
+              color: sb_dark_blue_50,
+            },
+          })}
+          placeholder="Title"
+          onChange={setTitle}
+          value={props.content.title}
+        />
+      </div>
+      <CodeMirror
+        css={css(
+          content.lineStates.map((state, index) => ({
+            [`.cm-gutterElement:nth-child(${index + 2})`]: state !== '' && {
+              backgroundColor: mix(
+                backgroundColorFromState(state),
+                sb_dark_blue,
+                0.5,
+              ),
+              color: gutterColorFromState(state),
+            },
+            [`.cm-line:nth-child(${index + 1})`]: state !== '' && {
+              backgroundColor: mix(
+                backgroundColorFromState(state),
+                sb_dark_blue,
+                0.25,
+              ),
+            },
+          })),
+        )}
+        initialValue={code}
+        onChange={onChange}
+        onLineNumberClick={handleLineNumberClick}
+      />
+    </div>
   )
 }
