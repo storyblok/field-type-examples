602afed38ce020a45e6ba70a21eaf9be4d3c5b38
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-23 09:52:47 +0200

    refactor(code-block): extracted CodeEditorHeader.tsx

--- code-block/src/components/CodeEditor/CodeEditor.tsx
+++ code-block/src/components/CodeEditor/CodeEditor.tsx
@@ -1,24 +1,16 @@
-import { ChangeEventHandler, FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 import { CodeEditorContent } from './CodeEditorContent'
 import { toggleLine } from './toggleLine'
 import { CodeMirror } from '../CodeMirror'
 import { mix } from './mix'
-import {
-  sb_dark_blue,
-  sb_dark_blue_50,
-  sb_dark_blue_hover,
-  sb_green,
-  transition,
-  white,
-} from '../../storyblok-design'
 import { css } from '@emotion/react'
-import { integerFromString } from '../../utils/numberFromString'
 import {
   defaultHighlightStateOption,
   HighlightStateOptions,
 } from '../../Options'
 import { colorFromHighlightState } from './colorFromHighlightState'
 import { withLength } from '../../utils'
+import { CodeEditorHeader } from './CodeEditorHeader'
 
 /**
  * A simple code editor without syntax highlighting where the user can select rows in four states: default, highlight, add, remove,
@@ -50,21 +42,17 @@ export const CodeEditor: FunctionComponent<{
       highlightStates: toggleLine(highlightStatesOption, highlightStates, line),
     })
 
-  const setTitle: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const { value } = e.currentTarget
+  const handleTitleChange = (title: string | undefined) =>
     setContent({
       ...content,
-      title: value === '' ? undefined : value,
-    } satisfies CodeEditorContent)
-  }
+      title,
+    })
 
-  const setLineNumberOffset: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const { value } = e.currentTarget
+  const handleLineNumberStartChange = (lineNumberStart: number | undefined) =>
     setContent({
       ...content,
-      lineNumberStart: integerFromString(value),
-    } satisfies CodeEditorContent)
-  }
+      lineNumberStart,
+    })
 
   return (
     <div
@@ -76,42 +64,12 @@ export const CodeEditor: FunctionComponent<{
         overflow: 'hidden',
       })}
     >
-      <div
-        css={css({
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor: sb_dark_blue,
-          color: white,
-        })}
-      >
-        <FieldSet
-          css={{
-            flex: 1,
-          }}
-        >
-          <input
-            id="title"
-            placeholder="Title"
-            onChange={setTitle}
-            value={content.title}
-          />
-        </FieldSet>
-        <FieldSet>
-          <label htmlFor="lineNumberStart">Starts at:</label>
-          <input
-            id="lineNumberStart"
-            type="number"
-            min={1}
-            css={css({
-              appearance: 'textfield',
-              width: '5ch',
-            })}
-            placeholder="1"
-            onChange={setLineNumberOffset}
-            value={content.lineNumberStart}
-          />
-        </FieldSet>
-      </div>
+      <CodeEditorHeader
+        title={content.title}
+        onTitleChange={handleTitleChange}
+        lineNumberStart={content.lineNumberStart}
+        onLineNumberStartChange={handleLineNumberStartChange}
+      />
       <CodeMirror
         css={css(
           content.highlightStates.map((state, index) => ({
@@ -141,57 +99,3 @@ export const CodeEditor: FunctionComponent<{
     </div>
   )
 }
-
-const FieldSet: FunctionComponent<
-  React.DetailedHTMLProps<
-    React.FieldsetHTMLAttributes<HTMLFieldSetElement>,
-    HTMLFieldSetElement
-  >
-> = (props) => (
-  <fieldset
-    css={css({
-      display: 'flex',
-
-      border: 'none',
-      margin: 0,
-      padding: 0,
-      color: 'inherit',
-      backgroundColor: 'inherit',
-      '& > *': {
-        padding: '7px 15px',
-        border: 'none',
-        borderBottom: `1px solid ${sb_dark_blue_50}`,
-        transition: transition('background-color', 'border-color'),
-        // Typography
-        fontSize: '12px',
-        fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-        fontWeight: 400,
-        lineHeight: '1.66',
-        letterSpacing: '0.03333em',
-      },
-      '& > label': {
-        color: sb_dark_blue_50,
-      },
-      '& > input': {
-        flex: 1,
-        color: 'inherit',
-        backgroundColor: 'inherit',
-
-        '&::placeholder': {
-          color: sb_dark_blue_50,
-        },
-        '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
-          '-webkit-appearance': 'none',
-        },
-      },
-      '& > input:focus': {
-        borderColor: sb_green,
-        outline: 'none',
-      },
-      '&:hover input': {
-        backgroundColor: sb_dark_blue_hover,
-      },
-    })}
-    {...props}
-  />
-)
--- code-block/src/components/CodeEditor/index.ts
+++ code-block/src/components/CodeEditor/index.ts
@@ -1,2 +1,1 @@
 export * from './CodeEditor'
-export { colorFromHighlightState } from './colorFromHighlightState'
