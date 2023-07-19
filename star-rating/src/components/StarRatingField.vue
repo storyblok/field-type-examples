7216ac37c4cd9796d26e3d93f739270ea0b5f0f9
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-07-18 21:19:07 -0300

    EXT-1850: Import the new StarIcon component

--- star-rating/src/components/StarRatingField.vue
+++ star-rating/src/components/StarRatingField.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { useFieldPlugin } from '../useFieldPlugin'
+import StarIcon from './StarIcon.vue'
 
 const plugin = useFieldPlugin()
 
@@ -53,24 +54,10 @@ watch(model, (newValue) => {
 <template>
   <ul @mouseleave="onMouseLeave" role="radiogroup" class="rate">
     <li :class="getStarValue(index)" v-for="index in numberOfStars" :key="index">
-      <div role="radio" :aria-checked="model > index ? 'true' : 'false'" :aria-posinset="index + 1" :aria-setsize="numberOfStars"
+      <div role="radio" :aria-checked="model > index" :aria-posinset="index + 1" :aria-setsize="numberOfStars"
         tabindex="0">
-        <div @click="onClick(index - 0.5)" @mousemove="onHover(index - 0.5)" class="rate__star--first">
-          <svg viewBox="64 64 896 896" class="rate__icon" width="1em" height="1em" fill="currentColor" aria-hidden="true"
-            focusable="false">
-            <path
-              d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z">
-            </path>
-          </svg>
-        </div>
-        <div @click="onClick(index)" @mousemove="onHover(index)" class="rate__star--second">
-          <svg viewBox="64 64 896 896" class="rate__icon" width="1em" height="1em" fill="currentColor" aria-hidden="true"
-            focusable="false">
-            <path
-              d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z">
-            </path>
-          </svg>
-        </div>
+        <StarIcon class="rate__star--first" @click="onClick(index - 0.5)" @mousemove="onHover(index - 0.5)" />
+        <StarIcon class="rate__star--second" @click="onClick(index)" @mousemove="onHover(index)" />
       </div>
     </li>
   </ul>
