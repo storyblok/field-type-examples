9d4b11de4d9f51e133c5114db0d341284d517f2a
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-07-31 08:57:16 -0300

    Rename tags to tags-legacy

---
+++ tags-legacy/src/components/TagsPlugin.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="tags-container">
+    <vue-tags-input
+      :value="inputValue"
+      :tags="tags"
+      :placeholder="options.placeholder || ''"
+      @input="handleInputValueChange"
+      @tags-changed="handleValueChange"
+    >
+      <div slot="tag-actions" slot-scope="props">
+        <i @click="props.performDelete">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="0.0857849"
+              y="1.5"
+              width="2"
+              height="9"
+              rx="1"
+              transform="rotate(-45 0.0857849 1.5)"
+              fill="#1B243F"
+            />
+            <rect
+              x="6.44975"
+              y="0.0858307"
+              width="2"
+              height="9"
+              rx="1"
+              transform="rotate(45 6.44975 0.0858307)"
+              fill="#1B243F"
+            />
+          </svg>
+        </i>
+      </div>
+    </vue-tags-input>
+  </div>
+</template>
+<script>
+import VueTagsInput from '@johmun/vue-tags-input'
+
+export default {
+  components: {
+    VueTagsInput,
+  },
+  props: {
+    options: {
+      type: Object,
+      default: undefined,
+    },
+    setValue: {
+      type: Function,
+    },
+    value: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      inputValue: '',
+    }
+  },
+  computed: {
+    tags() {
+      return this.value.value?.map((text) => ({ text }))
+    },
+  },
+  methods: {
+    handleInputValueChange(inputValue) {
+      this.inputValue = inputValue
+    },
+    handleValueChange(tags) {
+      this.setValue({ value: tags.map((tag) => tag.text) })
+    },
+  },
+}
+</script>
+
+<style>
+html {
+  font-family: 'Roboto', sans-serif;
+  font-size: 62.5%;
+}
+
+.tags-container {
+  padding: 2px;
+}
+
+.vue-tags-input .ti-input:hover {
+  border-color: #00b3b0;
+}
+
+.vue-tags-input.ti-focus .ti-input {
+  border-radius: 5px;
+  border-color: #00b3b0;
+  box-shadow: 0 0 0 3px #d9f4f3;
+  outline: none;
+}
+
+.vue-tags-input .ti-input {
+  border: 1px solid #dfe3e8;
+  width: 100%;
+  color: #1b243f;
+  font-size: 12px;
+  border-radius: 5px;
+  transition: all 0.1s ease-in-out;
+  padding: 10px 12px;
+  font-family: 'Roboto', sans-serif;
+}
+
+.vue-tags-input .ti-tag.ti-tag.ti-deletion-mark {
+  background-color: #bfc2c5;
+}
+
+.vue-tags-input .ti-tag {
+  background-color: #dfe3e8;
+  padding: 3px 10px;
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  padding: 3px 10px;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 5px;
+  margin: 0 8px 4px 0;
+  color: #1b243f;
+}
+
+.vue-tags-input .ti-tag .ti-actions {
+  margin-left: 10px;
+}
+
+.vue-tags-input .ti-tag .ti-icon-close:before {
+  content: url('data:image/svg+xml; utf8, <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.0857849" y="1.5" width="2" height="9" rx="1" transform="rotate(-45 0.0857849 1.5)" fill="#1B243F"/><rect x="6.44975" y="0.0858307" width="2" height="9" rx="1" transform="rotate(45 6.44975 0.0858307)" fill="#1B243F"/></svg>');
+}
+</style>
