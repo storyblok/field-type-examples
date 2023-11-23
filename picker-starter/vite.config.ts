cb8af54eb66feb11776c1ee5ffbbbf4f48bd5b3f
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-11-23 13:17:26 -0300

    add tests

--- picker-starter/vite.config.ts
+++ picker-starter/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
@@ -6,6 +7,11 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
   plugins: [vue(), cssInjectedByJsPlugin(), ...plugins],
   resolve: {
     alias: {
