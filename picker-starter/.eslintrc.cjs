b1e3bf507cb8839c118ee88b1cf2bb69b34a2cc8
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-11-23 22:11:55 -0300

    fix prettier

--- picker-starter/.eslintrc.cjs
+++ picker-starter/.eslintrc.cjs
@@ -1,57 +1,56 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true,
-      jest: true,
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  overrides: [
+    {
+      files: '*.ts',
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {}, // Override rules as well for TS files. Basically any config option can be modified here
     },
-    overrides: [
-      {
-        files: '*.ts',
+    {
+      files: '*.vue',
+      parser: 'vue-eslint-parser',
+      parserOptions: {
         parser: '@typescript-eslint/parser',
-        extends: ['plugin:@typescript-eslint/recommended'],
-        rules: {}, // Override rules as well for TS files. Basically any config option can be modified here
+        sourceType: 'module',
       },
-      {
-        files: '*.vue',
-        parser: 'vue-eslint-parser',
-        parserOptions: {
-          parser: '@typescript-eslint/parser',
-          sourceType: 'module',
-        },
-        extends: ['plugin:vue/vue3-recommended'],
-        rules: {
-          'vue/multi-word-component-names': 'off',
-          'vue/html-self-closing': [
-            'warn',
-            {
-              html: {
-                void: 'always',
-              },
+      extends: ['plugin:vue/vue3-recommended'],
+      rules: {
+        'vue/multi-word-component-names': 'off',
+        'vue/html-self-closing': [
+          'warn',
+          {
+            html: {
+              void: 'always',
             },
-          ],
-        }, // Override rules as well for TS files. Basically any config option can be modified here
+          },
+        ],
+      }, // Override rules as well for TS files. Basically any config option can be modified here
+    },
+  ],
+
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    curly: 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
       },
     ],
-  
-    extends: ['eslint:recommended'],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-      parser: 'babel-eslint',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-      curly: 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          args: 'none',
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  }
-  
+  },
+}
