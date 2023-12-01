c3a28d9ff5d35d4711d5e53d89a18937f7881048
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-12-01 13:30:59 -0300

    only show pagination component when there is something to paginate

--- picker-starter/src/components/ItemPicker/utils/mixin/showPagePagination.ts
+++ picker-starter/src/components/ItemPicker/utils/mixin/showPagePagination.ts
@@ -1,6 +1,8 @@
+import { defaultPerPage } from '@/settings'
 import { State } from '../state/types/State'
 import { isPagePaginatedResult } from '@/core'
 
 export const showPagePagination = (state: State): boolean =>
   isPagePaginatedResult(state.pageInfo) &&
-  typeof state.pageInfo.totalCount !== 'undefined'
+  Number.isInteger(state.pageInfo.totalCount) &&
+  state.pageInfo.totalCount > defaultPerPage
