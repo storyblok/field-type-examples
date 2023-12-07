88bebef83c55a8e690cc258922b6d412368a2477
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-12-07 19:03:56 -0300

    return service from defineConfig

--- picker-starter/src/core/setup.ts
+++ picker-starter/src/core/setup.ts
@@ -1,5 +1,5 @@
 import { Component } from 'vue'
-import { OptionsParams, TabItem } from './types'
+import { OptionsParams, PickerPluginParams, TabItem } from './types'
 
 export type PickerConfig = {
   title?: string
@@ -20,7 +20,32 @@ export type ValidationResult = {
 
 export type PickerConfigFn = (options: OptionsParams) => PickerConfig
 
+export type PickerBuilderFn = (options: OptionsParams) => PickerPluginParams
+
 export const defineConfig =
-  (fn: PickerConfigFn): PickerConfigFn =>
-  (options: OptionsParams) =>
-    fn(options)
+  (fn: PickerConfigFn): PickerBuilderFn =>
+  (options: OptionsParams): PickerPluginParams => {
+    const config: PickerConfig = fn(options)
+
+    return {
+      title: config.title,
+      icon: config.icon,
+      makeService: () => {
+        const { options } = config
+
+        const validation = options.validate()
+
+        if (!validation.isValid) {
+          return {
+            exampleOptions: options.example,
+            error: validation.error,
+          }
+        }
+
+        return {
+          exampleOptions: options.example,
+          value: { tabs: config.tabs },
+        }
+      },
+    }
+  }
