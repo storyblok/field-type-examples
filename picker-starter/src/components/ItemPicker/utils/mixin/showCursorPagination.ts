4a5f9147cbb0d60bc4ffeb24356dfad45dc0bc3f
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2024-06-14 11:46:56 -0300

    fix: load more button appearing even when there is no result to show (#95)

--- picker-starter/src/components/ItemPicker/utils/mixin/showCursorPagination.ts
+++ picker-starter/src/components/ItemPicker/utils/mixin/showCursorPagination.ts
@@ -2,5 +2,4 @@ import { State } from '../state/types/State'
 import { isCursorPaginatedResult } from '@/core'
 
 export const showCursorPagination = (state: State): boolean =>
-  isCursorPaginatedResult(state.pageInfo) &&
-  typeof state.pageInfo.endCursor !== 'undefined'
+  isCursorPaginatedResult(state.pageInfo) && state.pageInfo.hasNextPage
